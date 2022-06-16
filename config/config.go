c40c3ae6f0ccd1f16e3d711fcc52ef1a6fed48d8
Shoaib <falakshabir123@gmail.com>
2022-06-17 00:56:11 +0530

    first production ready

--- config/config.go
+++ config/config.go
@@ -1,17 +1,14 @@
 package config
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func Config(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Println("env load error", err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Println("env load error", err)
+	// }
 	return os.Getenv(key)
 
 }
